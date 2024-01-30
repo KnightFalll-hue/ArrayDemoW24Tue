530ec5ee806187c3cc2ce5748a4f4932a24538ce
KnightFalll-hue <57510488+KnightFalll-hue@users.noreply.github.com>
2024-01-30 12:30:00 -0500

    Update Student.java

--- src/arraydemo/Student.java
+++ src/arraydemo/Student.java
@@ -7,7 +7,7 @@ package arraydemo;
 /**
  *This has been modified by michael on 23 jan at 12:47pm
  * This is another commit added on 12.54pm
- * @author michael hello there, this is for a pull
+ * @author michael hello there, this is for a pull, fetch+merge
  */
 public class Student {
     private int sid;
